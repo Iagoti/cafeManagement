01948def6fcc38daa658d7a3e9f9ae3812bc4dfd
Iagoti <iagoti03@gmail.com>
2024-05-22 00:54:40 -0300

    Print

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connection = require('./connection');
 const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
 const productRoute = require('./routes/product');
+const billRoute = require('./routes/bill');
 const app = express();
 
 app.use(cors());
@@ -11,7 +12,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/category', categoryRoute);
-app.use('/product', productRoute)
+app.use('/product', productRoute);
+app.use('/bill', billRoute);
 
 app.listen(3000, function() {
     console.log('App listening on port 3000!');
