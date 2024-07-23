d6f3034a705d9c42cb46a5b92dc1c3fe58cf278f
Iagoti <iagoti03@gmail.com>
2024-07-22 22:54:06 -0300

    Criado botão sair(encerrar sessão)

--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ConfirmationComponent } from 'src/app/material-component/dialog/confirmation/confirmation.component';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,22 @@ import { Router } from '@angular/router';
 })
 export class AppHeaderComponent {
   role: any;
+
   constructor(private router: Router,
     private dialog: MatDialog) {
+  }
 
+  logout(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: 'Logout'
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user)=>{
+      dialogRef.close();
+      localStorage.clear();
+      this.router.navigate(['/']);
+    });
   }
 
 }
